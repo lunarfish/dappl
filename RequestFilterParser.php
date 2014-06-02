0a7d54eebd99f404284691349d97bb67e5e6f72e
Rick Peacock <rickopeacock@gmail.com>
2014-06-02 14:13:26 +0100

    Filter parser working for example requests

--- RequestFilterParser.php
+++ RequestFilterParser.php
@@ -7,9 +7,6 @@
  */
 
 $f = array();
-$f[] = <<< 'HEREDOC'
-(LocationID ge 1234) and (endswith(PostCode,'XT') eq true)
-HEREDOC;
 
 $f[] = <<< 'HEREDOC'
 ((((((LocationID eq 1234) and (endswith(PostCode,'XT') eq true)) and (substringof('a single quote ''yes''',Address1) eq true)) and (startswith(Address2,'double%22 single'' quotes') eq true)) and (Address3 ne 'double%22 only')) and (Address4 eq 'single'' only')) and (Town eq 'percent %2522 twentytwo')
@@ -23,28 +20,276 @@ $f[] = <<< 'HEREDOC'
 ((LocationID ge 1234) and (endswith(PostCode,'XT') eq true)) and (Address1 eq 'single'' double" percent% hello')
 HEREDOC;
 
+$f[] = <<< 'HEREDOC'
+(LocationID ge 1234) and (endswith(PostCode,'XT') eq true)
+HEREDOC;
+
+
+
+
+echo 'TEST (property equals string value) predicate - will fail at the moment' . PHP_EOL;
+
 
 
 $scanner = new RequestFilterTokenizer();
+$parser = new RequestFilterPredicateParser();
 foreach($f as $input) {
+    // Create token list
     $tokens = $scanner->tokenize($input);
-    echo $input . PHP_EOL;
+
+    // Output
+    /*echo $input . PHP_EOL;
+    $line = 0;
     foreach($tokens as $token) {
-        echo $token;
+        echo "($line) $token";
+        $line++;
+    }
+    echo PHP_EOL . PHP_EOL;
+*/
+
+    // Create predicate list
+    $predicates = $parser->getPredicates($tokens);
+
+    // Output
+    echo $input . PHP_EOL;
+    foreach($predicates as $predicate) {
+        echo $predicate . PHP_EOL;
     }
     echo PHP_EOL . PHP_EOL;
 }
 
 
 
-
-class RequestFilterParser
+class RequestFilterPredicate
 {
+    private $property;
+    private $operator;
+    private $value;
 
 
-    public function tokenize($input)
+    public function __construct($property, $operator, $value)
     {
+        $this->property = $property;
+        $this->operator = $operator;
+        $this->value = $value;
+    }
 
+
+    public function __toString()
+    {
+        return sprintf('(%s %s %s)', $this->property, $this->operator, $this->value);
+    }
+}
+
+
+/**
+ * Massive simplification - we are only dealing with the AND binary operator so we are not processing binary operations
+ * on the predicates. Just parsing predicates and returning them in order of appearance.
+ * To save on implementing the full recursive decent parser, save that fun for another day :-)
+ *
+ * Any 'OR' found will trigger an exception.
+ *
+ * Class RequestFilterPredicateParser
+ */
+class RequestFilterPredicateParser
+{
+    private $tokens;
+    private $predicates;
+
+
+    public function getPredicates(array $tokens)
+    {
+        // Set and reset token pointer
+        $this->tokens = $tokens;
+        reset($this->tokens);
+
+        // Create output
+        $this->predicates = array();
+
+        // Iterate tokens and process in global scope
+        while($this->hasMoreTokens()) {
+            // Handle token
+            $token = $this->getNextToken();
+            switch($token->getType()) {
+                case RequestFilterToken::OPEN_BRACKET:
+                    $this->eatPredicate();
+                    break;
+
+                case RequestFilterToken::CLOSE_BRACKET:
+                case RequestFilterToken::BINARY:
+                    // do nothing, ignoring AND/OR processing for now.
+                    break;
+
+                default:
+                    throw new Exception('Found wrong token in global scope:' . $token);
+                    break;
+            }
+        }
+
+        return $this->predicates;
+    }
+
+
+    private function pushPredicate(RequestFilterToken $property, RequestFilterToken $operator, RequestFilterToken $value)
+    {
+        $predicate = new RequestFilterPredicate($property->getValue(), $operator->getValue(), $value->getValue());
+        $this->predicates[] = $predicate;
+    }
+
+
+    private function eatPredicate()
+    {
+        // Get the next token
+        if ($this->hasMoreTokens()) {
+            $token = $this->getNextToken();
+            switch($token->getType()) {
+                case RequestFilterToken::OPEN_BRACKET:
+                    // Wind back and do nothing, - when the recursive descendant parser is implemented here we will push a new scope onto the stack and pass processing to that
+                    $this->getPrevToken();
+                    break;
+
+                case RequestFilterToken::COMMAND:
+                    $this->eatCommandPredicate($token);
+                    break;
+
+                case RequestFilterToken::STRING:
+                    $this->eatPropertyPredicate($token);
+                    break;
+
+                default:
+                    throw new Exception(sprintf('Found illegal token: [%s] in method: %s from token: %s', $token, __METHOD__, $this->currentTokenIndex()));
+            }
+        } else {
+            throw new Exception('Ran out of tokens during ' . __METHOD__);
+        }
+    }
+
+
+    /**
+     * Consume, validate and create a command predicate
+     * eg (endswith(PostCode,'XT') eq true)
+     */
+    private function eatCommandPredicate($commandToken)
+    {
+        $expectedTypes = array(array(
+            RequestFilterToken::OPEN_BRACKET,
+            RequestFilterToken::STRING,
+            RequestFilterToken::COMMA,
+            RequestFilterToken::QUOTED_STRING,
+            RequestFilterToken::CLOSE_BRACKET,
+            RequestFilterToken::OPERATOR,
+            RequestFilterToken::BOOL,
+            RequestFilterToken::CLOSE_BRACKET
+        ), array(
+            RequestFilterToken::OPEN_BRACKET,
+            RequestFilterToken::QUOTED_STRING,
+            RequestFilterToken::COMMA,
+            RequestFilterToken::STRING,
+            RequestFilterToken::CLOSE_BRACKET,
+            RequestFilterToken::OPERATOR,
+            RequestFilterToken::BOOL,
+            RequestFilterToken::CLOSE_BRACKET
+        ));
+
+        // If we are handling substringof, it has different expectations
+        $mode = ('substringof' == $commandToken->getValue()) ? 1 : 0;
+
+        // We already have the command token to start us off.
+        $tokenList = array($commandToken);
+        $start = $this->currentTokenIndex();
+
+        // Eat tokens and validate against our expected list
+        foreach($expectedTypes[$mode] as $expectedType) {
+            if ($this->hasMoreTokens()) {
+                // Check token
+                $token = $this->getNextToken();
+                $type = $token->getType();
+                if ($type !== $expectedType) {
+                    throw new Exception(sprintf('%s: Found incorrect token type: %s, expected: %s at %s processing %s' , __METHOD__, $type, $expectedType, $start, $commandToken));
+                }
+
+                // Store
+                $tokenList[] = $token;
+            } else {
+                throw new Exception(sprintf('%s: Ran out of tokens at %s processing %s'), __METHOD__, $start, $commandToken);
+            }
+        }
+
+        // Create predicate from token list
+        if ($mode) {
+            $this->pushPredicate($tokenList[4], $tokenList[0], $tokenList[2]);
+        } else {
+            $this->pushPredicate($tokenList[2], $tokenList[0], $tokenList[4]);
+        }
+    }
+
+
+    /**
+     * Consume, validate and create a property predicate
+     * eg (LocationID ge 1234)
+     */
+    private function eatPropertyPredicate($propertyToken)
+    {
+        $expectedTypes = array(
+            RequestFilterToken::OPERATOR,
+            RequestFilterToken::STRING,
+            RequestFilterToken::CLOSE_BRACKET
+        );
+
+        // We already have the property token to start us off.
+        $tokenList = array($propertyToken);
+        $start = $this->currentTokenIndex();
+
+        // Eat tokens and validate against our expected list
+        foreach($expectedTypes as $i => $expectedType) {
+            if ($this->hasMoreTokens()) {
+                // Check token
+                $token = $this->getNextToken();
+                $type = $token->getType();
+
+                // Fail if type is not expected type. Allow either string or quoted string for the second element
+                if ($type !== $expectedType && (1 == $i) && ($type !== RequestFilterToken::QUOTED_STRING)) {
+                    throw new Exception(sprintf('%s: Found incorrect token type: %s, expected: %s at %s processing %s' , __METHOD__, $type, $expectedType, $start, $propertyToken));
+                }
+
+                // Store
+                $tokenList[] = $token;
+            } else {
+                throw new Exception(sprintf('%s: Ran out of tokens at %s processing %s'), __METHOD__, $start, $propertyToken);
+            }
+        }
+
+        // Create predicate from token list
+        $this->pushPredicate($tokenList[0], $tokenList[1], $tokenList[2]);
+    }
+
+
+    private function getNextToken()
+    {
+        // Grab token
+        $token = current($this->tokens);
+
+        // Advance pointer
+        next($this->tokens);
+        return $token;
+    }
+
+
+    private function getPrevToken()
+    {
+        prev($this->tokens);
+    }
+
+
+    private function hasMoreTokens()
+    {
+        return null !== key($this->tokens);
+    }
+
+
+    private function currentTokenIndex()
+    {
+        return key($this->tokens);
     }
 }
 
@@ -52,8 +297,8 @@ class RequestFilterParser
 
 class RequestFilterToken
 {
-    public $type;
-    public $value;
+    private $type;
+    private $value;
 
     const OPEN_BRACKET = '(';
     const CLOSE_BRACKET = ')';
@@ -70,6 +315,18 @@ class RequestFilterToken
     {
         $this->type = $type;
         $this->value = $value;
+    }
+
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+
+    public function getValue()
+    {
+        return $this->value;
     }
 
 
